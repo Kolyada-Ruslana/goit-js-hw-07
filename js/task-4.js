bd85d5ca420ec5d5233b19a0bda635ddc65b588f
Kolyada_Ruslana <ruslanamaxi297@gmail.com>
2025-09-21 22:18:32 +0300

    Оновлено проект: додано папки та файли

---
+++ js/task-4.js
@@ -0,0 +1,19 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const email = formEl.elements.email.value.trim();
+    const password = formEl.elements.password.value.trim();
+
+    if (email === "" || password === "") {
+        return alert('All form fields must be filled in')
+    }
+    const userForm = {
+        email: email,
+        password: password
+    }
+
+    console.log(userForm);
+
+    formEl.reset()
+});
